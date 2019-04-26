a6b64aedc6e12754534c01ac39477f9a33f23ee5
Anthony <anthony.marque@gmail.com>
2019-04-26 14:04:21 +0200

    added like only once logic.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,46 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "likes";
   isClicked: boolean = false;
   count = 0;
 
-  clickHandler() {
-    this.isClicked = !this.isClicked;
-    if (this.isClicked) this.count++;
-    else this.count--;
+  ngOnInit() {
+    if (this.count === 0) {
+      this.noLikeToggle();
+    }
   }
 
   likeHandler() {
     this.isClicked = true;
     if (this.isClicked) this.count++;
     else this.count--;
-    this.buttonToggle("like",true);
-    this.buttonToggle("noLike", false);
+    this.likeToggle();
   }
 
   noLikeHandler() {
     this.isClicked = false;
     if (!this.isClicked) this.count--;
     else this.count++;
-    this.buttonToggle("noLike", true);
-    this.buttonToggle("like", false);
+    this.noLikeToggle();
   }
 
-  buttonToggle(btnId: string, bool:boolean) {
+  buttonToggle(btnId: string, bool: boolean) {
     const element = document.getElementById(btnId);
     (<HTMLInputElement>element).disabled = bool;
+  }
+
+  likeToggle() {
+    this.buttonToggle("like", true);
+    this.buttonToggle("noLike", false);
+  }
+  noLikeToggle() {
+    this.buttonToggle("like", false);
+    this.buttonToggle("noLike", true);
   }
 }
