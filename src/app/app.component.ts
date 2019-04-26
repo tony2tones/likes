1162b9c5fc8a0f2c981b5e71fef0a39b2b08fc07
Anthony <anthony.marque@gmail.com>
2019-04-26 13:45:20 +0200

    added like toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,14 +17,23 @@ export class AppComponent {
   }
 
   likeHandler() {
-    this.isClicked = !this.isClicked;
+    this.isClicked = true;
     if (this.isClicked) this.count++;
     else this.count--;
+    this.buttonToggle("like",true);
+    this.buttonToggle("noLike", false);
   }
 
   noLikeHandler() {
-    this.isClicked = !this.isClicked;
-    if (this.isClicked) this.count--;
+    this.isClicked = false;
+    if (!this.isClicked) this.count--;
     else this.count++;
+    this.buttonToggle("noLike", true);
+    this.buttonToggle("like", false);
+  }
+
+  buttonToggle(btnId: string, bool:boolean) {
+    const element = document.getElementById(btnId);
+    (<HTMLInputElement>element).disabled = bool;
   }
 }
