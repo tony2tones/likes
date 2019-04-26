4ae205cc731f129760d563109f31273e43e9b872
Anthony <anthony.marque@gmail.com>
2019-04-26 10:29:23 +0200

    working on stylings and buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'likes';
+  title = "likes";
   isClicked: boolean = false;
   count = 0;
 
   clickHandler() {
-     this.isClicked = !this.isClicked;
-     console.log('the heart has been clicked', this.isClicked);
+    this.isClicked = !this.isClicked;
+    if (this.isClicked) this.count++;
+    else this.count--;
   }
 }
