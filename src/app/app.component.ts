d3bdc2724e4da52fc5aadb8ad1a8d51606abc6ce
Anthony <anthony.marque@gmail.com>
2019-05-20 09:12:00 +0200

    adjusted subscribe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { CounterService } from "./counter/counter.service";
+import { ConstantPool } from "@angular/compiler";
+import { Compiler_compileModuleSync__POST_R3__ } from "@angular/core/src/linker/compiler";
 
 const util = require("./util/util");
 
@@ -22,6 +24,7 @@ export class AppComponent implements OnInit {
   likeHandler() {
     this.isClicked = true;
     if (this.isClicked) this.count++;
+    this.updateCounter();
     util.buttonToggle("noLike", false);
   }
 
@@ -29,6 +32,7 @@ export class AppComponent implements OnInit {
     this.isClicked = false;
     if (this.count === 0) util.buttonToggle("noLike", true);
     else this.count--;
+    this.updateCounter();
   }
 
   getCount() {
@@ -39,5 +43,13 @@ export class AppComponent implements OnInit {
       },
       error => console.log(error)
     );
+  }
+
+  updateCounter() {
+    this.counterService
+      .postCounter(this.count)
+      .subscribe(
+        error => console.log(error)
+      );
   }
 }
